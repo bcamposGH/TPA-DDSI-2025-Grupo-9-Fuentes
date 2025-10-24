b237a3a72f07636c3f2da02fe5c4da2d8edf7b97
bcamposGH <bcampos@frba.utn.edu.ar>
2025-10-24 16:27:12 -0300

    Vuelta atras PdIDTO

--- src/main/java/ar/edu/utn/dds/k3003/facades/dtos/PdIDTO.java
+++ src/main/java/ar/edu/utn/dds/k3003/facades/dtos/PdIDTO.java
@@ -1,18 +1,55 @@
 package ar.edu.utn.dds.k3003.facades.dtos;
 
-import com.fasterxml.jackson.annotation.JsonAlias;
-import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import java.time.LocalDateTime;
 import java.util.List;
 
-@JsonIgnoreProperties(ignoreUnknown = true)
-public record PdIDTO(
-        String id,
-        @JsonAlias({"hecho_id"}) String hechoId,
-        String descripcion,
-        String lugar,
-        LocalDateTime momento,
-        @JsonAlias({"url_imagen"}) String urlImagen,
-        @JsonAlias({"texto_imagen"}) String textoImagen,
-        List<String> etiquetas
-) {}
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+public record PdIDTO(String id, @JsonProperty("hechoId") String hechoId, String descripcion, String lugar, LocalDateTime momento, String urlImagen, String textoImagen, List<String> etiquetas) {
+   public PdIDTO(String id, String hechoId) {
+      this(id, hechoId, (String)null, (String)null, (LocalDateTime)null, (String)null, (String)null, List.of());
+   }
+
+   public PdIDTO(String id, String hechoId, String descripcion, String lugar, LocalDateTime momento, String urlImagen, String textoImagen, List<String> etiquetas) {
+      this.id = id;
+      this.hechoId = hechoId;
+      this.descripcion = descripcion;
+      this.lugar = lugar;
+      this.momento = momento;
+      this.urlImagen = urlImagen;
+      this.textoImagen = textoImagen;
+      this.etiquetas = etiquetas;
+   }
+
+   public String id() {
+      return this.id;
+   }
+
+   public String hechoId() {
+      return this.hechoId;
+   }
+
+   public String descripcion() {
+      return this.descripcion;
+   }
+
+   public String lugar() {
+      return this.lugar;
+   }
+
+   public LocalDateTime momento() {
+      return this.momento;
+   }
+
+   public String urlImagen() {
+      return this.urlImagen;
+   }
+
+   public String textoImagen() {
+      return this.textoImagen;
+   }
+
+   public List<String> etiquetas() {
+      return this.etiquetas;
+   }
+}
